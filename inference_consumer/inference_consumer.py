0e4cc40feac40e4fd593a2422441bf790514f09f
Anders-EM <kurt.a.emrich@vanderbilt.edu>
2024-11-08 17:39:17 -0600

    inferencer

--- inference_consumer/inference_consumer.py
+++ inference_consumer/inference_consumer.py
@@ -7,24 +7,25 @@ import io
 import time
 
 # Kafka configuration
-BROKER_IP = '192.168.5.115:9092'
-TOPIC_INPUT = 'images'
-TOPIC_OUTPUT = 'results'
+BROKER_IP = "kafka:9092"
+TOPIC_INPUT = "images"
+TOPIC_OUTPUT = "results"
 
 # CouchDB configuration
-COUCHDB_URL = "http://admin:password@192.168.5.115:5984/images"
+COUCHDB_URL = "http://admin:password@192.168.5.34:31367/images"
 
 # Kafka consumer and producer setup
 consumer = KafkaConsumer(
     TOPIC_INPUT,
     bootstrap_servers=[BROKER_IP],
-    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+    value_deserializer=lambda x: json.loads(x.decode("utf-8")),
 )
 
 producer = KafkaProducer(
     bootstrap_servers=[BROKER_IP],
-    value_serializer=lambda x: json.dumps(x).encode('utf-8')
+    value_serializer=lambda x: json.dumps(x).encode("utf-8"),
 )
+
 
 def update_couchdb(image_id, predicted_class):
     doc_url = f"{COUCHDB_URL}/{image_id}"
@@ -32,46 +33,53 @@ def update_couchdb(image_id, predicted_class):
 
     if response.status_code == 200:
         doc = response.json()
-        doc['predicted_class'] = predicted_class
-        headers = {'Content-Type': 'application/json'}
-        update_response = requests.put(f"{doc_url}?rev={doc['_rev']}", headers=headers, data=json.dumps(doc))
+        doc["predicted_class"] = predicted_class
+        headers = {"Content-Type": "application/json"}
+        update_response = requests.put(
+            f"{doc_url}?rev={doc['_rev']}", headers=headers, data=json.dumps(doc)
+        )
 
         if update_response.status_code == 201:
             print(f"Document {image_id} successfully updated.")
         else:
-            print(f"Failed to update document {image_id}. Error: {update_response.text}")
+            print(
+                f"Failed to update document {image_id}. Error: {update_response.text}"
+            )
     else:
         print(f"Failed to retrieve document {image_id}. Error: {response.text}")
+
 
 def process_image_for_inference(image_data):
     img_array = np.array(image_data, dtype=np.uint8).reshape(32, 32, 3)
     image = Image.fromarray(img_array)
 
     image_buffer = io.BytesIO()
-    image.save(image_buffer, format='PNG')
+    image.save(image_buffer, format="PNG")
     image_buffer.seek(0)
 
     return image_buffer
 
+
 def send_for_inference(image_id, image_data):
     image_buffer = process_image_for_inference(image_data)
     response = requests.post(
-        "http://192.168.5.5:5000/infer", 
-        files={'file': ('image.png', image_buffer, 'image/png')}
+        "http://192.168.5.34:5000/infer",
+        files={"file": ("image.png", image_buffer, "image/png")},
     )
 
     if response.status_code == 200:
-        predicted_class = response.json()['predicted_class']
+        predicted_class = response.json()["predicted_class"]
         print(f"Inference result: {image_id} => {predicted_class}")
-        obj = {'ID': image_id, 'predicted_class': predicted_class}
-        producer.send(TOPIC_OUTPUT, value=bytes (json.dumps(obj), 'ascii'))
+        obj = {"ID": image_id, "predicted_class": predicted_class}
+        producer.send(TOPIC_OUTPUT, value=bytes(json.dumps(obj), "ascii"))
         update_couchdb(image_id, predicted_class)
     else:
         print(f"Failed inference for {image_id}. Error: {response.text}")
 
+
 for message in consumer:
-    image_id = message.value['ID']
-    image_data = message.value['data']
+    image_id = message.value["ID"]
+    image_data = message.value["data"]
     start_time = time.time()
 
     send_for_inference(image_id, image_data)
